ff1b2c8b1d598d447f6a1a62953674743e415fe5
Jean Fraga <jeandevjs@gmail.com>
2022-07-20 14:54:17 -0300

    feat: - add contructor vars, init method, start method and docsSetup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
+import { readFile } from 'fs/promises'
 import express from 'express';
 import Routes from './router.js';
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 
+import * as OpenApiValidator from 'express-openapi-validator';
 
-class App {
+const apiSchema = JSON.parse( await readFile(new URL('./api.schema.json', import.meta.url)));
+import swaggerUi from 'swagger-ui-express';
+
+
+ export class App {
     constructor(){
-        this.server = express();
-        this.middlewares();
-        this.routes();
+      this.url = process.env.URL  
+      this.port = process.env.PORT
+      this.appName = process.env.API_NAME
+      this.appVersion =  process.env.API_VERSION  
+    }
+
+    async init () {
+      this.server = express();
+      this.middlewares();
+      this.routes();
+      this.docsSetup()
     }
     
     middlewares(){
@@ -16,12 +30,24 @@ class App {
         this.server.use(bodyParser.urlencoded({ extended: false }));
     }
 
+    async docsSetup () {
+      this.server.use(`/${this.appName}/${this.appVersion}/docs`, swaggerUi.serve, swaggerUi.setup(apiSchema));
+      this.server.use(OpenApiValidator.middleware({ apiSpec: apiSchema }))
+    }
+
     routes(){
         this.server.use(new Router().get('/', (req,res)=> res.status(200).json({"AWS CONFIG":"OK"})));
-        this.server.use(`/${process.env.API_NAME}/${process.env.API_VERSION}`,Routes);
+        this.server.use(`/${this.appName}/${this.appVersion}`,Routes);
+    }
+
+    start () {
+      this.server.listen(this.port, () => console.log(`
+          Server runing on 🤓
+          http://${this.url}:${this.port}/${this.appName}/${this.appVersion}
+          `))
     }
 } 
 
-export default new App().server;
+
 
 
